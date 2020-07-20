fa33a9b8fa81d68ce23087615ebbe219a1d98e87
Marcos A. Silva <marcossilva26@gmail.com>
2020-07-20 20:41:21 -0300

    Conteudo ministrado no curso da Digital Innovation One

---
+++ CursoMVC/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "O campo descrição é obrigatório")]
+        public String Descricao { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+
+    }
+}
